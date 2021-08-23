80b5456a79cb0d233415ab97da83871651c62d94
Julio Cesar <juliocsrmf@gmail.com>
2021-08-23 20:52:24 -0300

    Consultas

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
+import { Op } from 'sequelize';
 
 import { Product } from '../models/Product';
 import { User } from '../models/User';
 
 export const home = async (req: Request, res: Response)=>{
 
-    let users = await User.findAll();
+    let users = await User.findAll({
+        where: {
+            age: {
+                [Op.gte]: 0
+            }
+        },
+        order: [
+            ['name', 'DESC']
+        ],
+        offset: 2,
+        limit: 2
+    });
 
     let age: number = 90;
     let showOld: boolean = false;
