41dac17ed72df5f1ec4cf72b3ce510890bf3fa1e
Julio Cesar <juliocsrmf@gmail.com>
2021-08-23 23:05:10 -0300

    Alterando dados com sequelize

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from 'express';
+import { Op } from 'sequelize';
 
 import { Product } from '../models/Product';
 import { User } from '../models/User';
 
-export const home = async (req: Request, res: Response)=>{
+export const home = async (req: Request, res: Response) => {
+
+    let results = await User.findAll({ where: { id: 6 } });
+
+    if(results.length > 0) {
+        let usuario = results[0];
+        usuario.name = 'Trocou a idade';
+        usuario.age++;
+        await usuario.save();
+    }
 
     let age: number = 90;
     let showOld: boolean = false;
 
-    if(age > 50) {
+    if (age > 50) {
         showOld = true;
     }
 
